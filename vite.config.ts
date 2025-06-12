15d338574044e5f70941255d7a70837c68f4db2f
Bakhtawar Memon <bakhtawermemon193@gmail.com>
2025-06-12 13:23:03 +0800

    new updatw

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/yjs/', // 👈 this is the key!
   plugins: [react(), tailwindcss()],
 })
